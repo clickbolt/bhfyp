9f4e676348f507b86baf6236a345014bd8b41048
Clickbolt <140876031+clickbolt@users.noreply.github.com>
2024-01-29 16:50:15 +0500

    Update whatsapp-button.js

--- whatsapp-button.js
+++ whatsapp-button.js
@@ -360,7 +360,7 @@ function whatsappButton({brandName:t="",buttonName:e="",brandSubtitleText:a="",w
   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
   <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
   <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
-</svg>
+</svg><p class="wa-cta-button-label font-header wa-cta-button-label-margin">WhatsApp</p>
       </div>
    </button>
    <div id="wa-cta-button-powered">
